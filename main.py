b4725b9b139a2e56ac945f84add50bcd716df2e3
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-26 16:12:39 -0400

    More learning how to use Flask and dynamically generated URLs

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+from flask import Flask, render_template
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+# If you import this file into another file, __name__ is dynamically replaced by this filename, i.e. main
+
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    # df = pandas.read_csv("data.csv")
+    # temperature = df.station(date)
+    temperature = 23
+    # return str(temperature)
+    return{"station": station,
+           "date": date,
+           "temperature": temperature}
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=True)
+else:
+    pass
