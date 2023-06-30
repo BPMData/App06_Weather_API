3156a3afa8279034543f89bae15e2e76ef4733e9
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-30 19:00:19 -0400

    Updated Historical Weather API to include URL endpoints for getting all the data for one particular station (which returns an enormous JSON), and slicing that data for one particular year.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ stations = stations.iloc[:,[0,1]]
 def home():
    return render_template("home.html", data=stations.to_html())
 
-@app.route("/api/v1/<station>/<date>")
+@app.route("/api/v1/<station>/<date>/")
 def about(station, date):
     filename = "datafiles/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
@@ -19,6 +19,33 @@ def about(station, date):
     return{"station": station,
            "date": date,
            "temperature in C": temperature}
+
+@app.route("/api/v1/<station>/")
+def all_data(station):
+    filename = "datafiles/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+"""
+Note we can re-use variable names like filename, df and station because we 
+only ever defined these as LOCAL variables within particular functions.
+"""
+
+@app.route("/api/v1/annual/<station>/<year>")
+def yearly(station, year):
+    filename = "datafiles/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    result = df.to_dict(orient="records")
+    # You could use the code:
+    # result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    # But that looks a little ridiculous IMO.
+
+    return result
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
